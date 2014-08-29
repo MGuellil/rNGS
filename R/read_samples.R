2083479443b1d266c4c4d2dff374eb6224d0c2e6
teasdalm <mattdt85@gmail.com>
2014-08-29 11:39:20 +0100

    update

---
+++ R/read_samples.R
@@ -0,0 +1,31 @@
+ #' Read Samples
+ #'
+ #' This function generates a dataframe to use for the NGS alignment
+ #' @param config file in csv format
+ #' @keywords NGS BWA
+ #' @export
+ #' @examples
+ #' read_samples(config_file_csv)
+
+
+read_samples <- function(config_file){
+  # function read_samples - reads the config csv with the samples
+  # to align creates a df for use with the rest of the program
+  #
+  # Arguments
+  # ==========
+  # config_file - file to read in csv format
+  #
+  # Returns
+  # =======
+  # data.frame samples_to_run_df - dataframe with info for the alignment
+  #
+  
+  samples_df <- read.csv(config_file, as.is = TRUE)
+  number_of_samples_read <- length(samples_df[,"Sample.Name"])
+  
+  # tell the user how many samples we have read
+  cat("Read ", number_of_samples_read, "sample/s for analysis\n") 
+  
+  return(samples_df)
+}
