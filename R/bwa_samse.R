2b2261172988d721084bbaeb265128ed97f6d979
Matthew Teasdale <matthew@teasdale.xyz>
2014-08-30 12:35:46 +0100

    update

--- R/bwa_samse.R
+++ R/bwa_samse.R
@@ -2,23 +2,15 @@
 #'
 #'R function to run the BWA "aln" method on our sample dataframe
 #'
-#'@param sample dataframe row
+#'@param fastq_file - path of fastq file to use in alignment
+#'@param genome - path of genome to use in alignment
 #'@keywords BWA samse
 #'@export
 #'@import tools
 #'@examples
 #'bwa_samse(sample_data_frame_row)
 
-bwa_samse <- function(config_df_row){
-  # bwa_samse a function to preform a BWA samse alingment
-  
-  # collect what I need
-  fastq_file <- config_df_row[1]
-  genome <- config_df_row[5]
-  output_file <- file_path_sans_ext(fastq_file)
-  sai_file <- paste(output_file, ".sai", sep = "")
-  sam_file <- paste(output_file, ".sam", sep = "")
-  read_groups <- paste("'", config_df_row[2], "'", sep = "")
+bwa_samse <- function(fastq_file, genome, read_groups, sai_file, sam_file){  
   
   # form my BWA samse string
   bwa_samse_string <- paste("bwa samse -r",
